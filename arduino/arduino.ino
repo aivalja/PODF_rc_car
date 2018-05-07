91bdf025c007a3f8f8b9d8731b836e9d4985be83
Anssi Tenhunen <anssi.tenhunen@gmail.com>
2018-05-07 13:12:50 +0300

    Move files

---
+++ arduino/arduino.ino
@@ -0,0 +1,14 @@
+void setup() {
+  // Setup all the pins
+  
+
+}
+
+void readReceivedBytes(){
+  // Read incoming bytes from bluetooth module and set variables accordingly
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
